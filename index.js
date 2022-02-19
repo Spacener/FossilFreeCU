0df9df3c4ecdf8c13580eceaa35bbfc49cd8a43d
Spacener <spst7341@colorado.edu>
2022-02-18 19:29:22 -0700

    new commit

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.get('/', function(req,res) {
     res.render(__dirname + '/home.ejs');
 })
 
+app.get('/about', function(req,res) {
+    res.render(__dirname + '/about.ejs');
+})
+
+app.get('/resources', function(req,res) {
+    res.render(__dirname + '/resources.ejs');
+})
+
+app.get('/app', function(req,res) {
+    res.render(__dirname + '/app.ejs');
+})
+
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
 })
